f7ab5b9b600956e9aa0a2945c2fa63a64883641e
tsakama <tsakama@10.4.2.188>
2017-09-07 22:10:19 +0900

    first commit

---
+++ src/hello_world/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package hello_world;
+
+/**
+ * @author tsakama
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello, world!");
+	}
+
+}
